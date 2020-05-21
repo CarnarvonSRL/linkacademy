669b6c296084659537ad4fed496f9fc74e1bdb9c
Cosmin <58934267+CarnarvonSRL@users.noreply.github.com>
2020-05-21 14:13:16 +0300

    Add files via upload

--- quiz/app.js
+++ quiz/app.js
@@ -1,5 +1,4 @@
-// Create a quiz object with a title and two questions.
-// A question has one or more answer, and one or more is valid.
+
 var quiz = {
     title: 'My quiz',
     questions: [
@@ -79,23 +78,18 @@ var quiz = {
     el: '#app',
     data: {
       quiz: quiz,
-      // Store current question index
+
       questionIndex: 0,
-      // An array initialized with "false" values for each question
-      // It means: "did the user answered correctly to the question n?" "no".
+
       userResponses: Array(quiz.questions.length).fill(false)
     },
-    // The view will trigger these methods on click
     methods: {
-      // Go to next question
       next: function() {
         this.questionIndex++;
       },
-      // Go to previous question
       prev: function() {
         this.questionIndex--;
       },
-      // Return "true" count in userResponses
       score: function() {
         return this.userResponses.filter(function(val) { return val }).length;
       }
